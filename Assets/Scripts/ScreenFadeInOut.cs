15ff61fd5785004c83a2bb0f9e91f9f127a3065b
Bonbon <meng_jiangtao@163.com>
2015-11-23 17:04:43 +0800

    lightMap baked, startscene layer.
    lightMap baked, startscene layer.

---
+++ Assets/Scripts/ScreenFadeInOut.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenFadeInOut : MonoBehaviour {
+
+	public float fadeSpeed = 0.5f;
+	public bool startScene = true;
+	private RawImage rawImage = null;
+
+	void Awake(){
+		rawImage = GetComponent<RawImage> ();
+	}
+
+	void Update () {
+		if (startScene) {
+			StartScene();
+		}
+	}
+
+	void FadeToClear(){
+		rawImage.color = Color.Lerp (rawImage.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+	void FadeToBlack(){
+		rawImage.color = Color.Lerp (rawImage.color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+
+	void StartScene(){
+		FadeToClear ();
+		if (rawImage.color.a <= 0.05f) {
+			rawImage.color = Color.clear;
+			rawImage.enabled = false;
+			startScene = false;
+		}
+	}
+
+	void EndScene(){
+		rawImage.enabled = true;
+		FadeToBlack ();
+		if (rawImage.color.a >= 0.95f) {
+			Application.LoadLevel(0);
+		}
+	}
+}
