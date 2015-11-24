74a5fef58e7121f60fd979badee3b786256a09c9
Bonbon <meng_jiangtao@163.com>
2015-11-24 17:06:53 +0800

    add laser
    add laser

---
+++ Assets/Scripts/LaserBlinking.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBlinking : MonoBehaviour {
+
+	public float onTime;
+	public float offTime;
+
+	private float time;
+	private Renderer laserRender;
+	private Light laserLight;
+	private BoxCollider laserCollider;
+
+	void Awake(){
+		laserRender = GetComponent<Renderer> ();
+		laserLight = GetComponent<Light> ();
+		laserCollider = GetComponent<BoxCollider> ();
+		time = 0f;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void switchTrigger(){
+		time = 0f;
+		laserRender.enabled = !laserRender.enabled;
+		laserLight.enabled = !laserLight.enabled;
+		laserCollider.enabled = !laserCollider.enabled;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time+= Time.deltaTime;
+		if (laserRender.enabled && time > onTime)
+			switchTrigger ();
+		if (!laserRender.enabled && time > offTime)
+			switchTrigger ();
+	}
+}
---
+++ Assets/Scripts/LaserPlayerDetection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserPlayerDetection : MonoBehaviour {
+
+
+	private GameObject _player;
+	private LastPlayerSight _lastPlayerSight;
+
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.FindWithTag (Tags.Player);
+		_lastPlayerSight = GameObject.FindWithTag (Tags.GameController).GetComponent<LastPlayerSight> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other){
+		if (GetComponent<Renderer> ().enabled) {
+			if(other.gameObject == _player){
+				_lastPlayerSight.position = other.transform.position;
+			}
+		}
+	}
+}
