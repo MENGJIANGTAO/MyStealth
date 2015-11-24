d39fd54a04136586cb4501bc8641d0b500cfc44c
Bonbon <meng_jiangtao@163.com>
2015-11-24 16:13:55 +0800

    add cams
    add cams

---
+++ Assets/Scripts/CCTVPlayerDetection.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CCTVPlayerDetection : MonoBehaviour {
+
+
+	private GameObject _player;
+	private LastPlayerSight _lastPlayerSight;
+
+
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.FindWithTag (Tags.Player);
+		_lastPlayerSight = GameObject.FindWithTag (Tags.GameController).GetComponent<LastPlayerSight> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject == _player) {
+			Vector3 realPlayerPos = _player.transform.position - transform.position;
+			RaycastHit hit;
+			if(Physics.Raycast(transform.position,realPlayerPos,out hit)){
+				if(hit.collider.gameObject == _player){
+					_lastPlayerSight.position = _player.transform.position;
+				}
+			}
+		}
+	}
+}
