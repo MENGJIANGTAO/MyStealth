88ee0c139096dd445507e77b3cf0c3687b4f7e5a
Bonbon <meng_jiangtao@163.com>
2015-11-25 16:51:37 +0800

    add laser trigger.
    add laser trigger.

---
+++ Assets/Scripts/LaserSwitchDeactivation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserSwitchDeactivation : MonoBehaviour {
+
+
+	public GameObject player;
+	public Material unlockedMat;
+	public GameObject laser;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LaserDeactivation(){
+		laser.SetActive (false);
+		Renderer screen = transform.Find ("prop_switchUnit_screen").GetComponent<Renderer> ();
+		screen.material = unlockedMat;
+		GetComponent<AudioSource> ().Play ();
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject == player) {
+			if(Input.GetButton("Switch")){
+				LaserDeactivation();
+			}
+		}
+	}
+}
