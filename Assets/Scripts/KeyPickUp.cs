54e982326da589b6691ae5085851aa1d874f62c5
Bonbon <meng_jiangtao@163.com>
2015-11-25 18:01:53 +0800

    Add key pickUp
    Add key pickUp

---
+++ Assets/Scripts/KeyPickUp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPickUp : MonoBehaviour {
+
+	public AudioClip _keyGrab;
+	private GameObject _player;
+	private PlayerInventory _playerInventory;
+
+	void Awake(){
+		_player = GameObject.Find ("player");
+		_playerInventory = _player.GetComponent<PlayerInventory> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject == _player) {
+			AudioSource.PlayClipAtPoint(_keyGrab,transform.position);
+			_playerInventory.hasKey = true;
+			Destroy(gameObject);
+		}
+	}
+}
